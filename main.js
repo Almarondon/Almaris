4ebc6613d66fe212e574a2d84f953aed66d75402
Almaris Rondon <almarisrondon@MacBook-Pro-de-Paulo.local>
2023-06-20 21:20:06 -0400

    add second commit

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const question2 = "¿Tienes una rutina de Skincare (si/no)?";
 
 const questions = [question1, question2];
 
-//va a recibir la respuesta y si es un si devuelve true, si es un no un false si es un null un null 
-//y si es cualquier otra cosa (sip, nop, nones) devuelve undefined
 function mapToBooleanValue(answer) {
     switch (answer) {
         case "si":
@@ -23,22 +21,18 @@ function simulator() {
     let continueWithQuestions = true;
     let requireContact = false;
 
-    //aqui recorre las dos preguntas 👇🏻 
     for (question of questions) {
-        //esto es para que si o si responda si o no si aprieta cancelar se sale
         while (true) {
-            //aqui se pregunta 
             const answer = prompt(question);
-            //la respuesta mapeada por la funcion
             const answerQuestion = mapToBooleanValue(answer);
-            // si es null significa que el usuario apreto cancelar
+    
             if (answerQuestion === null) {
                 continueWithQuestions = false;
                 break;
             }
-            //aqui significa que el usuario respondio que si o que no
+    
             else if (answerQuestion !== undefined) {
-                //si es false guardo que la contactemos
+        
                 if (answerQuestion === false) {
                     requireContact = true;
                 }
@@ -49,7 +43,7 @@ function simulator() {
             }
         }
 
-        //esto es para ver si me salgo o me quedo en el flujo, es decir si apreto cancelar o no
+
         if (continueWithQuestions) {
             continue;
         }
@@ -70,6 +64,4 @@ function simulator() {
     }
 }
 
-//Arranque del simulador
 simulator();
-
